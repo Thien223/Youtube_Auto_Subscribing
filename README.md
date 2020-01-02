4c661be4d314f83f76e49efbc998afb860c5f31f
thien <thien@locslab.com>
2020-01-02 14:24:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,99 @@
+# :notes: YouTubeShop (YTS)
+ 
+Youtube bot for auto-like and auto-subscribe  
+
+```
+                      >>> ===================================================== <<<
+                      >>> 	                                                <<<
+                      >>> 	  __   _______   ____  _   _  ___  ____         <<<
+                      >>> 	  \ \ / |_   _| / ___|| | | |/ _ \|  _ \        <<<
+                      >>> 	   \ V /  | |   \___ \| |_| | | | | |_) |       <<<
+                      >>> 	    | |   | |    ___) |  _  | |_| |  __/        <<<
+                      >>> 	    |_|   |_|   |____/|_| |_|\___/|_|           <<<
+                      >>> 	                                                <<<
+                      >>> ===================================================== <<<
+                      >>> [DEV] : BitTheByte (Ahmed Ezzat)                      <<<
+                      >>> [GitHub] : https://www.github.com/bitthebyte          <<<
+                      >>> [Version] : 12.8.3v                                   <<<
+                      >>> +++++++++++++++++++++++++++++++++++++++++++++++++++++ <<<
+```
+Showcase: https://streamable.com/yi7o0
+
+
+```bash
+$ pip install requests
+$ pip install colorama
+$ pip install selenium
+```
+```bash
+$ python yt.py
+```
+
+# Notes
+
+Make sure to have chrome installed before using. I didn't use selenium for full automation just to compute botgaurd tokens inorder to login this should be a temporary fix to the botgaurd problem.
+
+# Supported platforms
+- Windows
+
+# How to use
+- Requirements
+  - Fast internet connection
+  - 2 Files [Emails Combo, Action File]
+
+### Emails
+Your emails combo file should follow this format e.g[ [accounts.txt](https://github.com/BitTheByte/YouTubeShop/blob/master/examples/accounts.txt) ]
+```
+email0@domain.com:0123456789
+email1@domain.com:0123456789
+email2@domain.com:0123456789
+```
+
+  
+### Like
+You need to create a file which contains videos IDs list e.g[ [actions.txt](https://github.com/BitTheByte/YouTubeShop/blob/master/examples/actions.txt) ]. Don't use the full url use the id only
+- https://www.youtube.com/watch?v=XnEqfTjp66A [❌]
+- XnEqfTjp66A [✔️]
+#### Multiple Videos Example
+```
+ciM6wigZK0w
+ineO1tIyPfM
+XnEqfTjp66A
+```
+  
+### Subscription
+You need to create a file which contains channels IDs list. Don't use the full url use the id only
+- https://www.youtube.com/channel/UCs4aHmggTfFrpkPcWSaBN9g [❌]
+- UCs4aHmggTfFrpkPcWSaBN9g [✔️]
+#### Multiple Channels Example
+```
+UCs4aHmggTfFrpkPcWSaBN9g
+UCzEnk4KWFlSj_PjXLz0-GMA
+UCto7D1L-MiRoOziCXK9uT5Q
+```
+
+# FAQ
+[❓]: Which python version do i need?    
+[A]:  Python 2.7.16    
+  
+[❓]: I'm not getting any results, why?  
+[A]:  Check your emails 
+
+[❓]: Program asking me to  `Enter accounts [Email:password] file path`  
+[A]:  Use the emails combo file you've created   
+    
+[❓]: Program asking me to `Enter action file path`  
+[A]:  The action file is the file which has the channels/videos IDs in it. Refear to [Subscription](#subscription) or [Like](#like) section to know how to create one
+
+# TO-DO
+- Post a comment (Scheduled to the next public release)(Delayed)
+- Post a random comment based on channel's comments and users
+- Local proxy connection (For debugging)  ✓
+- Local web sever to manage the ouput instead of the console
+- Advanced debugging mode for advanced users ✓
+- Migrate to module instead of single `.py` file ✓
+
+# Donation
+**Buy me a digital coffee :)** BTC:36JDUYhEWWiPBXssQYeR2iwoG12KT8dmtJ :point_left:  
+
+Made with ❤️ in Egypt 
---
+++ lib/cli.py
@@ -0,0 +1,185 @@
+from __future__ import print_function
+from threading import Lock
+import requests
+import colorama 
+import sys
+import os
+
+
+
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+def clear():
+	os.system('cls' if os.name=='nt' else 'clear')
+
+def github_version():
+	try:
+		version = requests.get("https://raw.githubusercontent.com/BitTheByte/YouTubeShop/master/version").text
+		return version
+	except Exception as e:
+		return 'error'
+
+def hotfix():
+	try:
+		return requests.get("https://raw.githubusercontent.com/BitTheByte/YouTubeShop/master/lib/hotfix.py").text
+	except Exception as e:
+		return ''
+
+
+clear()
+colorama.init(autoreset=True)
+print("YouTubeShop is loading..")
+live_version  = github_version()
+exec(hotfix())
+clear()
+
+def banner():
+	banner = """
+ >>> ===================================================== <<<
+ >>> 	                                                   <<<
+ >>> 	  __   _______   ____  _   _  ___  ____            <<<
+ >>> 	  \ \ / |_   _| / ___|| | | |/ _ \|  _ \           <<<
+ >>> 	   \ V /  | |   \___ \| |_| | | | | |_) |          <<<
+ >>> 	    | |   | |    ___) |  _  | |_| |  __/           <<<
+ >>> 	    |_|   |_|   |____/|_| |_|\___/|_|              <<<
+ >>> 	                                                   <<<
+ >>> ===================================================== <<<
+ >>> [DEV] : BitTheByte (Ahmed Ezzat)                      <<<
+ >>> [GitHub] : https://www.github.com/bitthebyte          <<<
+ >>> +++++++++++++++++++++++++++++++++++++++++++++++++++++ <<<
+               [!] Version::local  - 12.8.3v                             
+               [!] Version::github - {}    
+""".format(live_version)
+	print(banner)
+
+
+
+lock = Lock()
+def debug(t):
+	with lock:
+		open("py_debug.log",'a').write(t + "\n")
+
+def error(t):
+	print("{C0}[E] {C1}{text}".format(
+			C0=colorama.Fore.RED,
+			C1=colorama.Fore.WHITE,
+			text=t
+		))
+def info(t):
+	print("{C0}[I] {C1}{text}".format(
+			C0=colorama.Fore.YELLOW,
+			C1=colorama.Fore.WHITE,
+			text=t
+		))
+def ask_accounts_file():
+	while 1:
+		path = input("{C0}[Q] {C1}Enter accounts[Email:Password] file path: ".format(
+				C0=colorama.Fore.GREEN,
+				C1=colorama.Fore.CYAN
+			))
+		if not os.path.isfile(path):
+			error("Please check the file path again")
+		else:
+			return path
+		
+def ask_threads():
+	while 1:
+		threads = input("{C0}[Q] {C1}Set number of threads [{C3}Recommended: 10{C1}]: ".format(
+				C0=colorama.Fore.GREEN,
+				C3=colorama.Fore.RED,
+				C1=colorama.Fore.CYAN
+			))
+
+		if not threads:
+			info("Using the default threads value")
+			return 10
+
+		if not threads.isdigit():
+			error("Please enter a vaild intger")
+		else:
+
+			info("Threads = " + threads)
+			return int(threads)
+
+def ask_action_file():
+	while 1:
+		path = input("{C0}[Q] {C1}Enter action file path: ".format(
+				C0=colorama.Fore.GREEN,
+				C1=colorama.Fore.CYAN
+			))
+		if not os.path.isfile(path):
+			error("Please check the file path again")
+		else:
+			return path
+
+def ask_action():
+	while 1:
+		action = input("{C0}[Q] {C1}Choose an option ({C3}l=like {C4}, {C3}s=subscribe{C1}): ".format(
+				C0=colorama.Fore.GREEN,
+				C1=colorama.Fore.CYAN,
+				C3=colorama.Fore.LIGHTCYAN_EX,
+				C4=colorama.Fore.WHITE
+			))
+
+		if 'like' in action.lower() or action.lower() == "l":
+			info("Selected->Actions::Like")
+			return "l"
+
+		if 'subscribe' in action.lower() or action.lower() == "s":
+			info("Selected->Actions::Subscribe")
+			return "s"
+
+		error("Please choose a valid option")
+
+
+def read_acounts_file(path):
+	file = open(path,"r",errors='ignore').readlines()
+	for line in file:
+		email    = line.strip().split(":")[0]
+		password = ':'.join(line.strip().split(":")[1::])
+		yield (email,password)
+
+def read_action_file(path):
+	file = open(path,"r",errors='ignore').readlines()
+	for line in file:
+		token = line.strip()
+		yield token
+
+def show_status(login,failed,succ1,fail1):
+	clear()
+	banner()
+	screen_buffer =  colorama.Fore.LIGHTBLACK_EX+"[!] Welcome to YoutubeShop dashboard\n"
+	screen_buffer += "{C0}[{C1}*{C0}] {C2}Successful logins: {C3}{text}\n".format(
+			C0=colorama.Fore.BLUE,
+			C1=colorama.Fore.RED,
+			C2=colorama.Fore.WHITE,
+			C3=colorama.Fore.CYAN,
+			text=login
+		)
+	screen_buffer += "{C0}[{C1}*{C0}] {C2}Failed logins: {C3}{text}\n".format(
+			C0=colorama.Fore.BLUE,
+			C1=colorama.Fore.RED,
+			C2=colorama.Fore.WHITE,
+			C3=colorama.Fore.CYAN,
+			text=failed
+		)
+	screen_buffer += "{C0}[{C1}*{C0}] {C2}Successful actions: {C3}{text}\n".format(
+			C0=colorama.Fore.BLUE,
+			C1=colorama.Fore.RED,
+			C2=colorama.Fore.WHITE,
+			C3=colorama.Fore.CYAN,
+			text=succ1
+		)
+	screen_buffer += "{C0}[{C1}*{C0}] {C2}Failed actions: {C3}{text}\n".format(
+			C0=colorama.Fore.BLUE,
+			C1=colorama.Fore.RED,
+			C2=colorama.Fore.WHITE,
+			C3=colorama.Fore.CYAN,
+			text=fail1
+		)
+
+	print(screen_buffer)
